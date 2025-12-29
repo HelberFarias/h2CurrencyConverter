2f0dc80d6267dda8e87e5d1d0117f4bdf2ad3023
Helber Farias <helberfarias.ptpro@gmail.com>
2025-12-29 17:34:54 -0300

    feat: estrutura consumo api funcionando

---
+++ src/ApiClient.java
@@ -0,0 +1,31 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiClient {
+    Gson gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .create();
+
+    public void ConverterApi(String currencyy) throws IOException, InterruptedException {
+        String API_KEY = "75aad2a0ca3e34113fb202a7";
+        String currency = "BRL";
+        String endereco = "https://v6.exchangerate-api.com/v6/" +
+                API_KEY + "/latest/"
+                + currency;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String resposta = (String)response.body();
+        System.out.println(resposta);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bora pra cima mundão!");
+        ApiClient newApiClient = new ApiClient();
+        newApiClient.ConverterApi("BRL");
+        System.out.println(newApiClient);
+    }
 }
