82e029b9ee143dd971912be5ac07d9c43f8168cb
Helber Farias <helberfarias.ptpro@gmail.com>
2026-01-02 13:09:40 -0300

    criação da classe DataList para guardar os dados e expor comportamento em forma de lista

--- src/main/br/alura/challenge/h2currencyconverte/apiClient/ApiClient.java
+++ src/main/br/alura/challenge/h2currencyconverte/apiClient/ApiClient.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 public class ApiClient {
 
---
+++ src/main/br/alura/challenge/h2currencyconverte/currencyData/DataList.java
@@ -0,0 +1,12 @@
+package main.br.alura.challenge.h2currencyconverte.currencyData;
+import main.br.alura.challenge.h2currencyconverte.records.ExchangeData;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataList {
+    private ArrayList<ExchangeData> exchangeDataList = new ArrayList<>();
+
+    public ArrayList<ExchangeData> getExchangeDataList() {
+        return exchangeDataList;
+    }
+}
